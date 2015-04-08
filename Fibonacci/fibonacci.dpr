893e9d201260ada5462917d7d1173e2ddd05ad61
Erico Almeida <programador.erico@gmail.com>
2015-04-08 00:11:08 -0300

    Primeiro commmit. Teste de fibonacci

---
+++ Fibonacci/fibonacci.dpr
@@ -0,0 +1,13 @@
+program fibonacci;
+
+uses
+  Forms,
+  uPrincipal in '..\Forms\uPrincipal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
